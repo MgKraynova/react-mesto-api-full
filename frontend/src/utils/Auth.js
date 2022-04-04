9d139e2002be292abf470daa8dcac0a24989411e
Marina Kraynova <kraynovamg@gmail.com>
2022-04-04 18:55:11 +0300

    fix baze url

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.mesto-app.nomoredomains.work/';
+export const BASE_URL = 'https://api.mesto-app.nomoredomains.work';
 
 function checkResult(res) {
     if (res.ok) {
