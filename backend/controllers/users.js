ecab1e093acfdb3f08b541d394005a22db4cbe96
Marina Kraynova <kraynovamg@gmail.com>
2022-04-04 17:51:49 +0300

    fix bugs

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,6 +6,7 @@ const CastError = require('../errors/CastError');
 const ValidationError = require('../errors/ValidationError');
 const ConflictingRequest = require('../errors/ConflictingRequest');
 const UnauthorizedError = require('../errors/UnauthorizedError');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports.getAllUsers = (req, res, next) => {
   User.find({})
@@ -124,7 +125,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        'some-secret-key',
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
       res.send({ token });
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,3 @@
-const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
@@ -13,7 +12,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
     next(new UnauthorizedError('Для совершения действия вы должны пройти авторизацию'));
   }
